c3d24c8545206026088f37eaf4522ddf67f35e83
Velolia <veloliafw@gmail.com>
2026-03-12 14:24:32 +0700

    First
    First commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Velolia\Database\Seeder;
+
+use App\Models\Role;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //
+    }
+}
